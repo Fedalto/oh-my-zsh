1c8513fbfe88ec9eebab9afb83fde462b768f1fb
Leonardo S. P. Fedalto <lfedalto@gmail.com>
2011-10-17 22:14:37 -0200

    Remove aliases and load the ~/.aliases used by every sh.

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -3,13 +3,6 @@ setopt auto_name_dirs
 setopt auto_pushd
 setopt pushd_ignore_dups
 
-alias ..='cd ..'
-alias cd..='cd ..'
-alias cd...='cd ../..'
-alias cd....='cd ../../..'
-alias cd.....='cd ../../../..'
-alias cd/='cd /'
-
 alias 1='cd -'
 alias 2='cd +2'
 alias 3='cd +3'
@@ -34,11 +27,3 @@ cd () {
   fi
 }
 
-alias md='mkdir -p'
-alias rd=rmdir
-alias d='dirs -v | head -10'
-
-# mkdir & cd to it
-function mcd() { 
-  mkdir -p "$1" && cd "$1"; 
-}
