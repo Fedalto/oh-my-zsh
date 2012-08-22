e2de66b5e4924c40aa37e8433402f5a278c93584
Leonardo S. P. Fedalto <lfedalto@gmail.com>
2012-08-22 12:10:10 -0300

    Renamed git.zsh to vcs.zsh

--- custom/vcs.zsh
+++ custom/vcs.zsh
@@ -14,4 +14,8 @@ zstyle ':vcs_info:*' enable git hg bzr svn
 # can be slow, comment it out if you encounter delays
 zstyle ':vcs_info:git:*' check-for-changes true
 
+# Set the formats
+zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f %F{6}%u%c%f '
+zstyle ':vcs_info:*' formats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f %F{6}%u%c%f '
+
 RPROMPT='${vcs_info_msg_0_}'
