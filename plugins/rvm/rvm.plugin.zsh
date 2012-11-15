0ec24f20bcbc648838c93aceb0b8bc582140926d
Leonardo Fedalto <lfedalto@gmail.com>
2012-11-14 22:28:12 -0200

    Changed rvm plugin

--- plugins/rvm/rvm.plugin.zsh
+++ plugins/rvm/rvm.plugin.zsh
@@ -2,33 +2,13 @@ if [ -d $HOME/.rvm/bin ]; then
 	PATH=$PATH:$HOME/.rvm/bin
 fi
 
+# Load RVM into a shell session *as a function*
+if [ -s "$HOME/.rvm/scripts/rvm" ]; then
+	source "$HOME/.rvm/scripts/rvm"
+fi
+
 alias rubies='rvm list rubies'
 alias gemsets='rvm gemset list'
-
-local ruby18='ruby-1.8.7-p334'
-local ruby19='ruby-1.9.2-p180'
-
-function rb18 {
-	if [ -z "$1" ]; then
-		rvm use "$ruby18"
-	else
-		rvm use "$ruby18@$1"
-	fi
-}
-
-_rb18() {compadd `ls -1 $rvm_path/gems | grep "^$ruby18@" | sed -e "s/^$ruby18@//" | awk '{print $1}'`}
-compdef _rb18 rb18
-
-function rb19 {
-	if [ -z "$1" ]; then
-		rvm use "$ruby19"
-	else
-		rvm use "$ruby19@$1"
-	fi
-}
-
-_rb19() {compadd `ls -1 $rvm_path/gems | grep "^$ruby19@" | sed -e "s/^$ruby19@//" | awk '{print $1}'`}
-compdef _rb19 rb19
 
 function rvm-update {
 	rvm get head
