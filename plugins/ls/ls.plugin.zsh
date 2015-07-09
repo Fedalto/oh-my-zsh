9fdb12f5e81ee26e63d20eb56502edabbcd456a6
Leonardo Fedalto <lfedalto@gmail.com>
2015-07-08 23:04:40 -0300

    Change options for ls

--- plugins/ls/ls.plugin.zsh
+++ plugins/ls/ls.plugin.zsh
@@ -1,14 +1,15 @@
 # ls colors
 autoload colors; colors;
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
-#export LS_COLORS
 
 # Enable ls colors
 if [ "$DISABLE_LS_COLORS" != "true" ]
 then
   # Find the option for using colors in ls, depending on the version: Linux or BSD
-  ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty -F' || alias ls='ls -GF'
+  ls --color -d . &>/dev/null 2>&1 && LS_COLOR_OPTION='--color=tty' || LS_COLOR_OPTION='-G'
 fi
+
+alias ls="ls $LS_COLOR_OPTION --dereference-command-line --human-readable --indicator-style=classify"
 
 # Typo?
 alias sl=ls
